dc5470e42a1eb836e03f787dbc1d88721171a599
Jamrsu <james_sutton@live.co.uk>
2023-01-11 14:30:36 +0000

    delete button

--- gui.py
+++ gui.py
@@ -7,10 +7,11 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+delete_button = sg.Button("Delete")
 
 window = sg.Window('My To-Do App',
                    layout=[[label], [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button], [delete_button]],
                    font=('Helvetica', 20))
 
 
@@ -34,6 +35,9 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Delete":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
